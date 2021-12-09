9acdfb24ccc915613b8462680f9c08bf6733ef31
nadialvy <nadia_lovely_29rpl@smktelkom-malang.sch.id>
2021-12-09 11:10:03 +0700

    filter component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // feature 1
 import React from 'react';
+import Filter from './components/Filter';
 import Products from './components/Products';
 import data from './data.json'; // ./ artinya mencari file
 
@@ -10,8 +11,50 @@ class App extends React.Component {
       products: data.products, //mengambil nilai products di dalam data .json
       size: "",
       sort: ""
-    }
+    };
   }
+
+  sortProducts = (event) =>{
+    //implement
+    const sort = event.target.value;
+
+    console.log(event.target.value); //read the value that user select
+    this.setState(state => ({
+      sort: sort,
+      products: this.state.products
+      .slice()
+      .sort((a, b) => 
+        sort === "lowest"
+        ? a.price > b.price
+          ? 1
+          : -1
+        : sort === "highest"
+        ? a.price < b.price
+          ? 1
+          : -1
+        : a._id > b._id
+          ? 1
+          : -1
+        ),
+    }));
+  };
+
+  filterProducts = (event) =>{
+    console.log(event.target.value); 
+    if(event.target.value === ""){
+      this.setState({
+        size: event.target.value,
+        products: data.products
+      });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+          ),
+      });
+    }
+  };
   
   render(){
     return (
@@ -24,6 +67,15 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+
+              ></Filter>
               <Products products={this.state.products}/>
             </div>
             <div className="sidebar">
